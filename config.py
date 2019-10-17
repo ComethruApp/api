687d068672f2a7d38f0db2d7198a56bc9cd0c2c4
ErikBoesen <me@erikboesen.com>
2019-10-17 17:18:59 -0400

    Add config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Override this in production')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+                                             'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ITEMS_PER_PAGE = 15
