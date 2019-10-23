e7b754471553fd7743af1e65d329f4145a82cffa
ErikBoesen <me@erikboesen.com>
2019-10-23 12:57:22 -0400

    Delete MeBots content

--- app/models.py
+++ app/models.py
@@ -66,11 +66,11 @@ class BlacklistedToken(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     token = db.Column(db.String(500), unique=True, nullable=False)
-    blacklisteded_on = db.Column(db.DateTime, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
 
     def __init__(self, token):
         self.token = token
-        self.blacklisteded_on = datetime.datetime.now()
+        self.blacklisted_on = datetime.datetime.now()
 
     def __repr__(self):
         return '<id: token: {}'.format(self.token)
