dfb4ab8c28b68936c7581abca62da6ba79b88a38
ErikBoesen <me@erikboesen.com>
2019-10-26 15:00:51 -0400

    Delete forms.py

--- app/forms.py
+++
@@ -1,4 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, TextAreaField, BooleanField, SelectField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Length, URL
-from app.models import User
