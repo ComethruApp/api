f63dd989bd32cb61eaa3718f7a1c759baf423ca6
ErikBoesen <me@erikboesen.com>
2019-11-02 21:39:04 -0400

    Add geography file

---
+++ app/geography.py
@@ -0,0 +1,20 @@
+from math import sin, cos, sqrt, atan2, radians
+
+# Approximate radius of earth in kilometers
+EARTH_RADIUS = 6373.0
+
+def distance(lat1, lng1, lat2, lng2):
+    lat1 = radians(lat1)
+    lng1 = radians(lng1)
+    lat2 = radians(lat2)
+    lng2 = radians(lng2)
+
+    dlng = lng2 - lng1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlng / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = EARTH_RADIUS * c
+
+    return distance
