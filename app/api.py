d865e8fcb16bbfc9260533c21595b5cb1a48049c
ErikBoesen <me@erikboesen.com>
2019-11-25 18:49:49 -0500

    Handle status code in utils too

--- app/api.py
+++ app/api.py
@@ -9,11 +9,11 @@ api_blueprint = Blueprint('api', __name__)
 
 @api_blueprint.errorhandler(404)
 def not_found_error(error):
-    return fail('Not found.'), 404
+    return fail('Not found.', 404)
 
 @api_blueprint.errorhandler(401)
 def unauthorized(error):
-    return fail('You don\'t have permission to do this.'), 401
+    return fail('You don\'t have permission to do this.', 401)
 
 @api_blueprint.before_request
 def verify_token():
@@ -26,48 +26,48 @@ def get_user(user_id):
     user = User.query.get(user_id)
     if user is None:
         abort(404)
+    return jsonify(user.json(g.me))
+
+@api_blueprint.route('/users/me')
+def get_me():
+    return jsonify(g.me.json(g.me))
 
 @api_blueprint.route('/users/search/<query>')
 def search_users(query):
     users = User.query.filter(User.id != g.me.id, User.name.ilike('%' + query + '%')).all()
-    return jsonify([user.json() for user in users])
-
-@api_blueprint.route('/users/me')
-def get_me():
-    return jsonify(g.me.json())
+    return jsonify([user.json(g.me) for user in users])
 
 @api_blueprint.route('/events')
 def get_events():
     events = Event.query.all()
-    return jsonify([event.json() for event in events])
+    return jsonify([event.json(g.me) for event in events])
 
 @api_blueprint.route('/events/<event_id>')
 def get_event(event_id):
     event = Event.query.get(event_id)
     if event is None:
         abort(404)
-    return jsonify(event.json())
+    return jsonify(event.json(g.me))
 
 @api_blueprint.route('/events', methods=['POST'])
 def create_event():
-    data = request.get_json()
+    data = request.get_json(g.me)
     event = Event(data, school_id=g.me.school_id)
     event.add_host(g.me)
     db.session.add(event)
     db.session.commit()
-    print(event.json())
-    return jsonify(event.json())
+    return jsonify(event.json(g.me))
 
 @api_blueprint.route('/events/<event_id>', methods=['PUT'])
 def update_event(event_id):
-    data = request.get_json()
+    data = request.get_json(g.me)
     event = Event.query.get(event_id)
     if event.hosted_by(g.me):
         # TODO: evaluate security concerns...
         for key, value in data.items():
             setattr(event, key, value)
         db.session.commit()
-        return jsonify(event.json()), 202
+        return jsonify(event.json(g.me)), 202
     else:
         abort(401)
 
@@ -81,25 +81,28 @@ def delete_event(event_id):
     # FIXME: this fails because we haven't gotten rid of the hostships
     db.session.delete(event)
     db.session.commit()
-    return succ('Event deleted successfully.'), 200
+    return succ('Event deleted successfully.')
 
 @api_blueprint.route('/events/<event_id>/invites')
 def get_event_invitees(event_id):
     event = Event.query.get(event_id)
     if event is None:
         abort(404)
-    return jsonify([user.json() for user in event.invitees])
+    return jsonify([user.json(g.me) for user in event.invitees])
 
 @api_blueprint.route('/events/<event_id>/invites/<user_id>', methods=['POST'])
 def create_invitation(event_id, user_id):
     event = Event.query.get(event_id)
     user = User.query.get(user_id)
     if event.hosted_by(g.me):
-        event.invitees.append(user)
-        db.session.commit()
-        return succ('Invited user.'), 201
+        # Check out that intuitive syntax. Glorious. Like washing machines.
+        if event.invite(user):
+            db.session.commit()
+            return succ('Invited user.')
+        else:
+            return fail('User already invited.')
     else:
-        return fail('You\'re not allowed to invite people to this event.'), 403
+        return fail('You\'re not allowed to invite people to this event.', 403)
 
 @api_blueprint.route('/events/<event_id>/invites/<user_id>', methods=['DELETE'])
 def rescind(event_id, user_id):
@@ -108,13 +111,13 @@ def rescind(event_id, user_id):
     if event.hosted_by(g.me):
         event.invitees.delete(user)
         db.session.commit()
-        return succ('Rescinded user.'), 200
+        return succ('Rescinded user.', 200)
     else:
-        return fail('You\'re not allowed to manage invitations for this event.'), 403
+        return fail('You\'re not allowed to manage invitations for this event.', 403)
 
 @api_blueprint.route('/location', methods=['POST'])
 def update_location():
-    payload = request.get_json()
+    payload = request.get_json(g.me)
     g.me.lat = payload['lat']
     g.me.lng = payload['lng']
     # TODO: this is massively inefficient
@@ -129,20 +132,30 @@ def friend_request(target_id):
     target = User.query.get(target_id)
     if g.me.friend_request(target):
         db.session.commit()
-        return succ('Succesfully sent friend request!'), 201
+        return succ('Succesfully sent friend request!')
     else:
-        return fail('You\'re already friends with this person.'), 400
+        return fail('You\'re already friends with this person.')
+
+@api_blueprint.route('/friends/cancel/<target_id>', methods=['POST'])
+def friend_cancel(target_id):
+    friend_request_sent = g.me.friend_requests_sent.filter(friend_requests.c.friended_id == target_id).first()
+    if friend_request_sent is None:
+        return fail('Couldn\'t find a friend request to this person.')
+    if friend_request_sent is not None:
+        g.me.friend_requests_sent.remove(friend_request_sent)
+    db.session.commit()
+    return succ('Succesfully cancelled friend request.')
 
 @api_blueprint.route('/friends/accept/<friender_id>', methods=['POST'])
 def friend_accept(friender_id):
     req = g.me.friend_requests_received.filter(friend_requests.c.friender_id == friender_id).first()
     if req is None:
-        return fail('This person hasn\'t sent you a friend request.'), 400
+        return fail('This person hasn\'t sent you a friend request.')
     friend = User.query.get(friender_id)
     friend.friended.append(g.me)
     g.me.friend_requests_received.remove(req)
     db.session.commit()
-    return succ('Accepted the request!'), 200
+    return succ('Accepted the request!')
 
 # TODO: should this maybe use the DELETE verb?
 @api_blueprint.route('/friends/reject/<user_id>', methods=['POST'])
@@ -152,26 +165,26 @@ def friend_reject(user_id):
     """
     req = g.me.friend_requests_received.filter(friend_requests.c.friender_id == user_id).first()
     if req is None:
-        return fail('This person hasn\'t sent you a friend request.'), 400
+        return fail('This person hasn\'t sent you a friend request.')
     g.me.friend_requests_received.remove(req)
     db.session.commit()
-    return succ('Successfully rejected request.'), 200
+    return succ('Successfully rejected request.')
 
 @api_blueprint.route('/friends/remove/<user_id>', methods=['POST'])
 def friend_remove(user_id):
     """
-    Remove friend request or friendship.
+    Remove friendship.
     """
     friendship_sent = g.me.friended.filter(friendships.c.friended_id == user_id).first()
     friendship_received = g.me.frienders.filter(friendships.c.friender_id == user_id).first()
     if friendship_sent is None and friendship_received is None:
-        return fail('Couldn\'t find a friend request from this person.'), 400
+        return fail('Couldn\'t find a friendship with this person.')
     if friendship_sent is not None:
         g.me.friended.remove(friendship_sent)
     if friendship_received is not None:
         g.me.frienders.remove(friendship_received)
     db.session.commit()
-    return succ('Succesfully removed friend.'), 200
+    return succ('Succesfully removed friend.')
 
 @api_blueprint.route('/friends')
 def get_friends():
@@ -179,7 +192,7 @@ def get_friends():
     Get friends of logged in user.
     """
     friends = g.me.friends()
-    return jsonify([user.json() for user in friends]), 200
+    return jsonify([user.json(g.me) for user in friends]), 200
 
 @api_blueprint.route('/friends/requests')
 def get_friend_requests():
