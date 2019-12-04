b5b05e05f560dd8d75c03a1fa23890d4034fa58b
ErikBoesen <me@erikboesen.com>
2019-12-04 12:24:57 -0500

    Filter out ended events properly in feed curation

--- app/models.py
+++ app/models.py
@@ -226,6 +226,7 @@ class User(db.Model):
             Event.time < now,
             Event.ended == False,
             now - EVENT_LENGTH < Event.time,
+            (Event.end_time == None) | (now < Event.end_time),
         )
         # Put private events first
         events = events.order_by(Event.open)
