a3ab0537110a822f9a6ee55a6f485d6c80620047
ErikBoesen <me@erikboesen.com>
2019-12-03 19:37:01 -0500

    Split closed events query

--- app/models.py
+++ app/models.py
@@ -207,12 +207,13 @@ class User(db.Model):
         return self.has_received_friend_request(user) or self.has_sent_friend_request(user)
 
     def feed(self):
-        closed_events = self.invited_to.filter(
+        my_closed_events = self.hosted.filter(
             Event.open == False,
         )
-        closed_events = closed_events.union(self.hosted.filter(
+        invited_closed_events = self.invited_to.filter(
             Event.open == False,
-        ))
+        )
+        closed_events = my_closed_events.union(invited_closed_events)
         open_events = Event.query.filter(
             Event.open == True,
             Event.school_id == self.school_id,
