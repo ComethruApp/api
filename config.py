58b8f4d260c6aa293ac2f1db4d8455098937ae6d
ErikBoesen <me@erikboesen.com>
2019-12-04 12:31:18 -0500

    Remove ITEMS_PER_PAGE config variable

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
                                              'sqlite:///' + os.path.join(basedir, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ITEMS_PER_PAGE = 15
 
     BCRYPT_LOG_ROUNDS = 13
 
