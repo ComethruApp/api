d340fd04ddb0bc64c26adbcffb7ae89526656fcc
ErikBoesen <me@erikboesen.com>
2019-12-17 19:48:48 -0500

    Use Mailgun variables in config

--- app/mailgun.py
+++ app/mailgun.py
@@ -4,7 +4,8 @@ from app import app
 def send_email(to, subject, html):
     return requests.post('https://api.mailgun.net/v3/mail.comethru.io/messages',
                          auth=('api', app.config['MAILGUN_API_KEY']),
-                         data={'from': 'Comethru <hello@comethru.io>',
+                         data={'from': '{name} <{address}>'.format(name=app.config['MAILGUN_SENDER_NAME'],
+                                                                   address=app.config['MAILGUN_SENDER']),
                                'to': [to],
                                'subject': subject,
                                'html': html})
