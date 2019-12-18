5cf1f9ed65bd7e785230b7a5ff064260fb675ce0
ErikBoesen <me@erikboesen.com>
2019-12-17 19:49:04 -0500

    Clean up old Flask-Mail variables in config

--- config.py
+++ config.py
@@ -15,16 +15,6 @@ class Config(object):
 
     CONFIRMATION_TOKEN_EXPIRATION = 36000
 
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-
-    # gmail authentication
-    MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
-    MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
-
-    # mail accounts
-    MAIL_DEFAULT_SENDER = 'hello@comethru.io'
-
-    MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
+    MAILGUN_API_KEY = os.environ['MAILGUN_API_KEY']
+    MAILGUN_SENDER_NAME = 'Comethru'
+    MAILGUN_SENDER = 'hello@comethru.io'
