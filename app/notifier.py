bb360dcd1000a7527d542e043850b68554b2fbda
ErikBoesen <me@erikboesen.com>
2019-12-24 13:17:25 -0500

    Send OneSignal API key

--- app/notifier.py
+++ app/notifier.py
@@ -5,14 +5,15 @@ from app import app
 class Notifier:
     def friend_request(self, user_from, user_to):
         r = requests.post('https://onesignal.com/api/v1/notifications',
-                             json={
+                          json={
                                  'app_id': app.config['ONESIGNAL_APP_ID'],
                                  'included_segments': [],
                                  'include_external_user_ids': str(user_to.id),
                                  'headings': {'en': 'New friend request'},
                                  'contents': {'en': user_from.name + ' has sent you a friend request on Comethru.'},
                                  'data': {'task': 'TODO'},
-                             })
+                          },
+                          headers={'Authorization': 'Basic ' + app.config['ONESIGNAL_API_KEY']})
         print(r.text)
         return r
 
