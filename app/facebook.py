afc7e556a99d236603615d04e7fbc9ed875060e5
ErikBoesen <me@erikboesen.com>
2019-12-24 18:33:23 -0500

    Add starting FB API wrapper

---
+++ app/facebook.py
@@ -0,0 +1,10 @@
+from app import app
+import facebook
+
+class Facebook:
+    graph = facebook.GraphAPI(access_token=app.config['FACEBOOK_API_TOKEN'], version='3.1')
+
+    def get_friends(self, facebook_user_id):
+        return self.graph.get_connections(id=str(facebook_user_id), connection_name='friends')
+
+facebook = Facebook()
