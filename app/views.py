7f48673bb7e0f498da298f0e32a349bd96ba22d0
ErikBoesen <me@erikboesen.com>
2020-01-15 21:36:34 -0500

    Fix saying email instead of chunk

--- app/views.py
+++ app/views.py
@@ -4,13 +4,13 @@ from app.models import User
 
 def email_to_name(email: str):
     email = email.split('@')[0]
-    chunks = [chunk[0].upper() + chunk[1:] for chunk in email.split('.') if email.isalpha()]
+    chunks = [chunk[0].upper() + chunk[1:] for chunk in email.split('.') if chunk.isalpha()]
     return ' '.join(chunks)
 
 @app.route('/stats')
 def stats():
     for user in User.query.all():
-        if user.name == 'Name missing':
+        if user.name == 'Name missing' or user.name == '':
             user.name = email_to_name(user.email)
     db.session.commit()
     return render_template('stats.html', users=User.query.count())
