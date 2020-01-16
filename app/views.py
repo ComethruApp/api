3e45b821bdf8f5c8dccf119c8330445c1a131a5f
ErikBoesen <me@erikboesen.com>
2020-01-15 21:28:35 -0500

    Fix names

--- app/views.py
+++ app/views.py
@@ -2,6 +2,14 @@ from flask import render_template
 from app import app, db
 from app.models import User
 
+def email_to_name(email: str):
+    email = email.split('@')[0]
+    chunks = [chunk[0].upper() + chunk[1:] for chunk in email.split('.') if email.isalpha()]
+    return name
+
 @app.route('/stats')
 def stats():
+    for user in Users.query.all():
+        user.name = email_to_name(user.email)
+    db.session.commit()
     return render_template('stats.html', users=User.query.count())
