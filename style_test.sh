61892704173b0d5c31b3b8271e20ce5a3d62aea0
ErikBoesen <me@erikboesen.com>
2020-01-21 20:31:05 -0500

    Ignore E712 in pycodestyle test

--- style_test.sh
+++ style_test.sh
@@ -2,4 +2,5 @@
 # E501 Line too long
 # E402 Import not at top of file
 # E265 block comment should start with '# '
-pycodestyle *.py app/*.py --ignore=E501,E402,E265
+# E712 comparison to booleans; this is conventional in SQLAlchemy queries
+pycodestyle *.py app/*.py --ignore=E501,E402,E265,E712
