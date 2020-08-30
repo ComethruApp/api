8606e0f239790ed8531e76f586615db23c5dcf2a
ErikBoesen <me@erikboesen.com>
2020-08-30 10:17:24 -0600

    Fix undefined reference to ACCESS_TOKEN

--- app/images.py
+++ app/images.py
@@ -12,8 +12,10 @@ def image_upload(data) -> str:
     :return: URL of image now hosted on GroupMe server.
     """
     headers = {
-        'X-Access-Token': ACCESS_TOKEN,
+        'X-Access-Token': GROUPME_ACCESS_TOKEN,
         'Content-Type': 'image/jpeg',
     }
     r = requests.post('https://image.groupme.com/pictures', data=data, headers=headers)
-    return r.json()['payload']['url']
+    url = r.json()['payload']['url']
+    print('Image hosted on GroupMe: ' + url)
+    return url
