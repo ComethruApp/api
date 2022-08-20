1f2579367e71b1fdc96a293dd6ece116e08a3c2e
ErikBoesen <me@erikboesen.com>
2022-08-20 16:28:50 -0700

    Update register_blueprint syntax

--- app/__init__.py
+++ app/__init__.py
@@ -35,9 +35,9 @@ if not app.debug:
     app.logger.info('App startup')
 
 from app import models, api, auth, views
-app.register_blueprint(api.api)
-app.register_blueprint(auth.auth, url_prefix='/auth')
+app.register_blueprint(api.api, name='api')
+app.register_blueprint(auth.auth, url_prefix='/auth', name='auth')
 
 # Legacy compatibility
 # TODO: remove
-app.register_blueprint(api.api, url_prefix='/api')
+app.register_blueprint(api.api, url_prefix='/api', name='api_legacy')
